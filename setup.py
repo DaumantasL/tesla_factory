03f7454fd2a7820bd771bb751eefe3976d420fd9
DaumantasL <75472196+DaumantasL@users.noreply.github.com>
2021-07-10 14:04:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(name='tesla',
       long_description = long_description,
       long_description_content_type="text/markdown",
 
-      install_requires = [], #dependencies for running package
+      #install_requires = [], #dependencies for running package
 
-      extras_require = { #dependencies for developing package
-            "dev": [
-                  "pytest>=3.7",
-                  ],
-      },
+      #extras_require = { #dependencies for developing package
+      #      "dev": [
+      #            "pytest>=3.7",
+      #            ],
+      #},
 
       url = "",
       author = "Daumantas Lipskis",
